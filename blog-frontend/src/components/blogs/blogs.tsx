73e142eb89bd0285be1bdc98a8584560b8ccb7bd
parampreet singh <parampreetsingh1092@gmail.com>
2024-01-23 15:36:49 +0530

    feat: connected frontend with the backend

--- blog-frontend/src/components/blogs/blogs.tsx
+++ blog-frontend/src/components/blogs/blogs.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useQuery} from "@apollo/client"
+import { GET_BLOGS } from "../graphql/queries"
 
-const blogs = () => {
+const Blogs = () => {
+  const {loading,data,error} = useQuery(GET_BLOGS);
+  if(loading) return <p>loading the data ....</p>
+  if(error) return <p>ERROR....</p>
+  console.log(data);
   return (
     <div>blogs</div>
-  )
+  )  
 }
 
-export default blogs
+export default Blogs;
---
+++ blog-frontend/src/components/graphql/queries.ts
@@ -0,0 +1,12 @@
+import {gql } from "@apollo/client"
+export const GET_BLOGS = gql`
+{
+    blogs{
+        id
+        title
+        user{
+            name
+        }
+    }
+}
+`
